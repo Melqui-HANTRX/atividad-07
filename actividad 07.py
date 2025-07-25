54ea9a7faed793452e92d22b5fa4adf9c5e7badb
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-24 19:29:49 -0600

    def analizarlistas, suma el promedio, x es una variable temporal, pasa por los numeros designados y comprueba si cumple o no cumple

--- actividad 07.py
+++ actividad 07.py
@@ -8,3 +8,9 @@ def ingresar_numeros():
 
 def analizarLalista(numeros):
     suma = sum(numeros)
+    promedios = suma / len(numeros) if numeros else 0
+    positivos = sum(1 for x in numeros if x > 0)
+    negativos = sum(1 for x in numeros if x < 0)
+    cerosencalificaciones = sum(1 for x in numeros if x == 0)
+    multiplosdel3 = sum(1 for x in numeros if x % 3 == 0)
+    return suma, promedios, positivos, negativos, cerosencalificaciones, multiplosdel3
